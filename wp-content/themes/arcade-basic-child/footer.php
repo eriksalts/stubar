c01618d3d2156659b325f8639c7898ce32537886
vagrant <vagrant@scotchbox>
2015-05-08 01:48:08 +0000

    Designed by link added and fixed

--- wp-content/themes/arcade-basic-child/footer.php
+++ wp-content/themes/arcade-basic-child/footer.php
@@ -15,7 +15,7 @@ $bavotasan_theme_options = bavotasan_theme_options();
 			<div class="row">
 				<div class="copyright col-lg-12">
 					<span class="pull-left"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'arcade' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
-					<span class="credit-link pull-right">Designed by <a href="#"></a> </span>
+					<span class="credit-link pull-right">Designed by <a href="http://eriksalts.me">Erik Saltsgaver</a> </span>
 				</div><!-- .col-lg-12 -->
 			</div><!-- .row -->
 		</div><!-- #footer-content.container -->
