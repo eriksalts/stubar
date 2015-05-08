e0df26c52091ad0f186a0ed7c7f423f18439b1e8
vagrant <vagrant@scotchbox>
2015-05-08 03:18:05 +0000

    vetran and bbb styling issue resolve

--- wp-content/themes/arcade-basic-child/who_we_are.php
+++ wp-content/themes/arcade-basic-child/who_we_are.php
@@ -12,33 +12,33 @@ get_header();
 				while ( have_posts() ) : the_post();
 					?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<h1 class="entry-title"><?php the_title(); 
-						
+						<h1 class="entry-title"><?php the_title();
+
 						?></h1>
 
 					    <div class="entry-content description clearfix">
 						    <?php the_content( __( 'Read more', 'arcade') ); ?>
-						    
+
 					    </div><!-- .entry-content -->
-					   
+
 					    <?php get_template_part( 'content', 'footer' ); ?>
 					</article><!-- #post-<?php the_ID(); ?> -->
 
 					<?php
 					// comments_template( '', true );
 				endwhile; ?>
-				<?php 
+				<?php
 			    $args = array(
 			        'post_type' => 'about_us',
 			        'orderby' => 'title',
 			        'order' => 'ASC'
 			    );
-			    $the_query = new WP_Query( $args );		    
+			    $the_query = new WP_Query( $args );
 			?>
-			<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>	
+			<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
 			<?php get_template_part( 'content', 'who_we_are' ); ?>
-			
+
 			<?php endwhile; endif; ?>
 			</div>
 
@@ -47,12 +47,12 @@ get_header();
 	<!-- <div class="container clearfix"> -->
 		<div class="row link-row">
 			<div id="veteran" class="col-md-6">
-				<a class="who-we-are-links" href="http://www.veteranownedbusiness.com" target="_blank" ><img class="link-images" src="http://www.veteranownedbusiness.com/images/banner_links/vob_120x60_blue_camo.png" alt="Veteran Owned Business Directory, Get your free listing, now!" border="0px" /></a>
+				<a class="who-we-are-links image-anchor" href="http://www.veteranownedbusiness.com" target="_blank" ><img class="link-images" src="http://www.veteranownedbusiness.com/images/banner_links/vob_120x60_blue_camo.png" alt="Veteran Owned Business Directory, Get your free listing, now!" border="0px" /></a>
 			</div>
 			<div id="bbb" class="col-md-6">
-				<a class="who-we-are-links" title="Click for the Business Review of Stuehlmeyer Renovations, a Contractor - Remodel & Repair in Glen Carbon IL" href="https://www.bbb.org/stlouis/business-reviews/contractor-remodel-and-repair/stuehlmeyer-renovations-in-glen-carbon-il-310360447#sealclick"><img class="link-images" alt="Click for the BBB Business Review of this Contractor - Remodel & Repair in Glen Carbon IL" style="border: 0;" src="https://seal-stlouis.bbb.org/seals/blue-seal-250-52-stuehlmeyerrenovations-310360447.png" /></a>
+				<a class="who-we-are-links image-anchor" title="Click for the Business Review of Stuehlmeyer Renovations, a Contractor - Remodel & Repair in Glen Carbon IL" href="https://www.bbb.org/stlouis/business-reviews/contractor-remodel-and-repair/stuehlmeyer-renovations-in-glen-carbon-il-310360447#sealclick"><img class="link-images" alt="Click for the BBB Business Review of this Contractor - Remodel & Repair in Glen Carbon IL" style="border: 0;" src="https://seal-stlouis.bbb.org/seals/blue-seal-250-52-stuehlmeyerrenovations-310360447.png" /></a>
 			</div>
-		</div>	
+		</div>
 	<!-- </div> -->
 
 <?php get_footer(); ?>
